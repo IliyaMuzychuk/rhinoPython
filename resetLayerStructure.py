5ad544fa1e3c2b5c8692ad44a897fe1c2e6e591f
IliyaMuzychuk <118945279+IliyaMuzychuk@users.noreply.github.com>
2023-09-26 09:18:41 -0700

    updates

--- resetLayerStructure.py
+++ resetLayerStructure.py
@@ -1,9 +1,11 @@
 import rhinoscriptsyntax as rs
+import scriptcontext as sc
+
 
 def resetLayerStructure():
     # Get all layers in the Rhino document
     layers = rs.LayerNames()
-    dic = {}
+    data = {}
 
     for layer in layers:
         
@@ -17,16 +19,34 @@ def resetLayerStructure():
         layer_print_width = rs.LayerPrintWidth(layer)
 
 
-        dic[layer_name] = {
+        data[layer_name] = {
 
             'layer color' : layer_color,
             'layer linetype' : layer_linetype,
             'layer print color' : layer_print_color,
             'layer print width' : layer_print_width,
         }
-        
     
-    print(dic)
+    return data
+
+def ImportLayersFromFileTest():
+
+    filename = 'Z:/BIM Standards/00_Rhino/Rhino Template/XXXXX.X_MODEL TEMPLATE.3dm'
+
+    #filename=rs.OpenFileName("Choose file to import layers")
+
+    if not filename: return
+
+    read_file=Rhino.FileIO.File3dm.Read(filename)
+
+    if read_file:
+
+        layer_list=read_file.AllLayers
+        print(layer_list)
+        for layer in layer_list:
+            sc.doc.Layers.Add(layer)
+
+ 
 
 if __name__ == "__main__":
-    print('hello world')
+    ImportLayersFromFileTest()      
